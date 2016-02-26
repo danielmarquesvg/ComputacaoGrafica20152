93a341a51dcdd05f699ba971540dc87a2c71fc19
AndersonRenoir <a.renoir.info@gmail.com>
2016-02-25 23:34:50 -0300

    Reta com algoritmo do Ponto Médio - Esboço
    Será necessário alterar o método para suportar a normalização e eventuais bugs.

--- src/interfaceGrafica/PainelDoBuffer.java
+++ src/interfaceGrafica/PainelDoBuffer.java
@@ -12,91 +12,125 @@ import java.awt.Rectangle;
 @SuppressWarnings("serial")
 public class PainelDoBuffer extends JPanel {
 
-	int coordeX,coordeY;
+	int coordeX, coordeY;
 	double xincAtual, yincAtual;
-	
-	
-	BufferedImage buffer = new BufferedImage(600,600,BufferedImage.TYPE_INT_RGB);
-	
+
+	BufferedImage buffer = new BufferedImage(600, 600, BufferedImage.TYPE_INT_RGB);
+
 	public PainelDoBuffer() {
-		
-		
+
 		setBounds(new Rectangle(0, 0, 600, 600));
 		setBorder(new LineBorder(new Color(0, 0, 0)));
 
 		repaint();
-	
-	}
-	 
-	//Coloca o buffer no painel
-	@Override
-	    protected void paintComponent(Graphics g) {
-	        super.paintComponent(g);
-	        buffer.createGraphics();
-	        
-	        g.drawImage(buffer, 0,0, null); // see javadoc for more info on the parameters            
-	    }
-	
 
-	//Colore todos os pixel do buffer para branco
-	
-	public void limparTela(){
-		
+	}
+
+	// Coloca o buffer no painel
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		buffer.createGraphics();
+
+		g.drawImage(buffer, 0, 0, null); // see javadoc for more info on the
+											// parameters
+	}
+
+	// Colore todos os pixel do buffer para branco
+
+	public void limparTela() {
+
 		for (int i = 0; i < 600; i++) {
-			
+
 			for (int j = 0; j < 600; j++) {
-				
-				buffer.setRGB(i, j,Color.WHITE.getRGB());
+
+				buffer.setRGB(i, j, Color.WHITE.getRGB());
 			}
 		}
 		repaint();
-		
+
 	}
-	
-	//Desenha as retas do plano cartesiano
-	public void desenharRetasPlano(){
-		//System.out.println("OI");
-		
+
+	// Desenha as retas do plano cartesiano
+	public void desenharRetasPlano() {
+		// System.out.println("OI");
+
 		for (int x = 0; x < 600; x++) {
-			
-			buffer.setRGB(x,300,Color.BLACK.getRGB());
+
+			buffer.setRGB(x, 300, Color.BLACK.getRGB());
 		}
-			
+
 		for (int y = 0; y < 600; y++) {
-				
-			buffer.setRGB(300, y,Color.BLACK.getRGB());
+
+			buffer.setRGB(300, y, Color.BLACK.getRGB());
 		}
-		
-		//repaint();
-		
+
+		// repaint();
+
 	}
-	
-	//Plota um pixel pela entrada do usuário
-	
-	public void plotarPixel(){
-		
-		try{
-			
-			buffer.setRGB(this.coordeX + 300, - this.coordeY + 300, Color.BLACK.getRGB() );
+
+	// Plota um pixel pela entrada do usuário
+
+	public void plotarPixel() {
+
+		try {
+
+			buffer.setRGB(this.coordeX + 300, -this.coordeY + 300, Color.BLACK.getRGB());
 			repaint();
-		
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Digite um número válido: X: 0 a 600 e Y: 0 a 600");
 		}
-		
+
 	}
-	
+
+	public void plotarRetaPontoMedio(int x1, int y1, int x2, int y2) {
+		int dx, dy, x, y, p, twoDy, twoDyMinusDx;
+		try {
+			dx = Math.abs(x2 - x1);
+			dy = Math.abs(y2 - y1);
+
+			p = 2 * dy - dx; /* Valor inicial de d */
+			twoDy = 2 * dy; /* Incremento de E */
+			twoDyMinusDx = 2 * (dy - dx); /* Incremento de NE */
+
+			if (x1 > x2) {
+				x = x2;
+				y = y2;
+				x2 = x1;
+			} else {
+				x = x1;
+				y = y1;
+			}
+
+			do {
+				buffer.setRGB(x + 300, -y + 300, Color.BLACK.getRGB());
+				x++;
+				if (p < 0) {
+					p += twoDy;
+				} else {
+					y++;
+					p += twoDyMinusDx;
+				}
+			} while (x < x2);
+
+			repaint();
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Ocorreu um erro ao desenhar a reta!");
+		}
+	}
 
 	// Método de plotar a reta
 	public void plotarRetaDDA(double x1, double y1, double x2, double y2) {
-		
+
 		try {
-		
+
 			int comprimento;
 			double x, y, xinc, yinc;
 
 			comprimento = (int) Math.abs(x2 - x1);
-		
+
 			if (Math.abs(y2 - y1) > comprimento) {
 				comprimento = (int) Math.abs(y2 - y1);
 			}
@@ -107,16 +141,17 @@ public class PainelDoBuffer extends JPanel {
 			xinc = Math.abs((x2 - x1) / comprimento);
 			yinc = Math.abs((y2 - y1) / comprimento);
 
-			if (x1 < x2) {  
-				
-				if (y1 < y2) { // Retas da esquerda para a direita, de baixo para cima
+			if (x1 < x2) {
+
+				if (y1 < y2) { // Retas da esquerda para a direita, de baixo
+								// para cima
 					do {
 						buffer.setRGB((int) Math.round(x) + 300, -(int) Math.round(y) + 300, Color.BLACK.getRGB());
 						x += xinc;
 						y += yinc;
 					} while (x <= x2 && y <= y2);
 
-				} else { // Retas da esquerda para a direita, de cima para baixo 					
+				} else { // Retas da esquerda para a direita, de cima para baixo
 					do {
 						buffer.setRGB((int) Math.round(x) + 300, -(int) Math.round(y) + 300, Color.BLACK.getRGB());
 						x += xinc;
@@ -124,17 +159,18 @@ public class PainelDoBuffer extends JPanel {
 					} while (x <= x2 && y >= y2);
 
 				}
-				
-			} else { 
-				 
-				if (y1 < y2) { // Retas da direita para a esquerda, de baixo para cima
+
+			} else {
+
+				if (y1 < y2) { // Retas da direita para a esquerda, de baixo
+								// para cima
 					do {
 						buffer.setRGB((int) Math.round(x) + 300, -(int) Math.round(y) + 300, Color.BLACK.getRGB());
 						x -= xinc;
 						y += yinc;
 					} while (x >= x2 && y <= y2);
 
-				} else { // Retas da direita para a esquerda, de cima para baixo					
+				} else { // Retas da direita para a esquerda, de cima para baixo
 					do {
 						buffer.setRGB((int) Math.round(x) + 300, -(int) Math.round(y) + 300, Color.BLACK.getRGB());
 						x -= xinc;
@@ -142,7 +178,7 @@ public class PainelDoBuffer extends JPanel {
 					} while (x >= x2 && y >= y2);
 
 				}
-	
+
 			}
 
 			repaint();
@@ -153,26 +189,28 @@ public class PainelDoBuffer extends JPanel {
 		}
 
 	}
-	
-	public int getXX(){
+
+	public int getXX() {
 		return coordeX;
 	}
-   
-	public int getYY(){
+
+	public int getYY() {
 		return coordeY;
 	}
 
-	public void setXX(int coordeX){
-		this.coordeX = coordeX ;
+	public void setXX(int coordeX) {
+		this.coordeX = coordeX;
 	}
-	
-	public void setYY(int coordeY){
-		this.coordeY = coordeY ;
+
+	public void setYY(int coordeY) {
+		this.coordeY = coordeY;
 	}
-	public double getXinc(){
+
+	public double getXinc() {
 		return xincAtual;
 	}
-	public double getYinc(){
+
+	public double getYinc() {
 		return yincAtual;
 	}
 }
